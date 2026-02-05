8d106367e3ab9c3ae3049eb91661c662cf25c131
A720\dam1 <rocamora29angel@gmail.com>
2026-02-05 19:24:19 +0100

    Práctica 1

--- POO_1dam/src/main/java/Practica1/Programa.java
+++ POO_1dam/src/main/java/Practica1/Programa.java
@@ -3,6 +3,8 @@ package Practica1;
 import java.util.ArrayList;
 
 public class Programa {
+    private static final int DEF_TEMPORADAS = 0;
+
     private String nombre;
     private Cadena cadena;
     private int temporadas;
@@ -10,8 +12,12 @@ public class Programa {
     private ArrayList<Invitado> listaInvitados;
     private Empleado director;
 
-    public Programa (String nombre) {
-
+    public Programa (String nombre, Cadena cadena) {
+        this.nombre = nombre;
+        this.cadena = cadena;
+        temporadas = DEF_TEMPORADAS;
+        listaEmpleados = new ArrayList<>();
+        listaInvitados = new ArrayList<>();
     }
 
     public String getNombre() {
